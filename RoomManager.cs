1797a0705bb9322e77d6daaad25f318d65468f4e
LexusNexus <131169483+LexusNexus0@users.noreply.github.com>
2025-02-24 09:59:12 +1100

    Add files via upload

--- RoomManager.cs
+++ RoomManager.cs
@@ -26,19 +26,98 @@ public class RoomManager : MonoBehaviour
         if (!StartRandomiser && enterRoom)
         {
             Debug.Log("Room number: " + roomNumber);
-            if (roomNumber == "16")
+            switch (roomNumber)
             {
-                player.GetComponent<DoorWork>().DoorLeftCode = "";
-                player.GetComponent<DoorWork>().DoorUpCode = "13";
-                player.GetComponent<DoorWork>().DoorRightCode = "17";
-                player.GetComponent<DoorWork>().DoorDownCode = "";
-            }
-            else if (roomNumber == "17")
-            {
-                player.GetComponent<DoorWork>().DoorLeftCode = "16";
-                player.GetComponent<DoorWork>().DoorUpCode = "14";
-                player.GetComponent<DoorWork>().DoorRightCode = "18";
-                player.GetComponent<DoorWork>().DoorDownCode = "";
+                case "1":
+                    player.GetComponent<DoorWork>().DoorLeftCode = "";
+                    player.GetComponent<DoorWork>().DoorUpCode = "";
+                    player.GetComponent<DoorWork>().DoorRightCode = "2";
+                    player.GetComponent<DoorWork>().DoorDownCode = "7";
+                    break;
+                case "2":
+                    player.GetComponent<DoorWork>().DoorLeftCode = "1";
+                    player.GetComponent<DoorWork>().DoorUpCode = "";
+                    player.GetComponent<DoorWork>().DoorRightCode = "3";
+                    player.GetComponent<DoorWork>().DoorDownCode = "";
+                    break;
+                case "3":
+                    player.GetComponent<DoorWork>().DoorLeftCode = "2";
+                    player.GetComponent<DoorWork>().DoorUpCode = "";
+                    player.GetComponent<DoorWork>().DoorRightCode = "";
+                    player.GetComponent<DoorWork>().DoorDownCode = "";
+                    break;
+                case "6":
+                    player.GetComponent<DoorWork>().DoorLeftCode = "";
+                    player.GetComponent<DoorWork>().DoorUpCode = "";
+                    player.GetComponent<DoorWork>().DoorRightCode = "7";
+                    player.GetComponent<DoorWork>().DoorDownCode = "10";
+                    break;
+                case "7":
+                    player.GetComponent<DoorWork>().DoorLeftCode = "6";
+                    player.GetComponent<DoorWork>().DoorUpCode = "1";
+                    player.GetComponent<DoorWork>().DoorRightCode = "";
+                    player.GetComponent<DoorWork>().DoorDownCode = "";
+                    break;
+                case "10":
+                    player.GetComponent<DoorWork>().DoorLeftCode = "";
+                    player.GetComponent<DoorWork>().DoorUpCode = "6";
+                    player.GetComponent<DoorWork>().DoorRightCode = "11";
+                    player.GetComponent<DoorWork>().DoorDownCode = "";
+                    break;
+                case "11":
+                    player.GetComponent<DoorWork>().DoorLeftCode = "10";
+                    player.GetComponent<DoorWork>().DoorUpCode = "";
+                    player.GetComponent<DoorWork>().DoorRightCode = "12";
+                    player.GetComponent<DoorWork>().DoorDownCode = "14";
+                    break;
+                case "12":
+                    player.GetComponent<DoorWork>().DoorLeftCode = "11";
+                    player.GetComponent<DoorWork>().DoorUpCode = "";
+                    player.GetComponent<DoorWork>().DoorRightCode = "";
+                    player.GetComponent<DoorWork>().DoorDownCode = "";
+                    break;
+                case "13":
+                    player.GetComponent<DoorWork>().DoorLeftCode = "";
+                    player.GetComponent<DoorWork>().DoorUpCode = "";
+                    player.GetComponent<DoorWork>().DoorRightCode = "14";
+                    player.GetComponent<DoorWork>().DoorDownCode = "16";
+                    break;
+                case "14":
+                    player.GetComponent<DoorWork>().DoorLeftCode = "13";
+                    player.GetComponent<DoorWork>().DoorUpCode = "11";
+                    player.GetComponent<DoorWork>().DoorRightCode = "15";
+                    player.GetComponent<DoorWork>().DoorDownCode = "17";
+                    break;
+                case "15":
+                    player.GetComponent<DoorWork>().DoorLeftCode = "14";
+                    player.GetComponent<DoorWork>().DoorUpCode = "";
+                    player.GetComponent<DoorWork>().DoorRightCode = "";
+                    player.GetComponent<DoorWork>().DoorDownCode = "";
+                    break;
+                case "16":
+                    player.GetComponent<DoorWork>().DoorLeftCode = "";
+                    player.GetComponent<DoorWork>().DoorUpCode = "13";
+                    player.GetComponent<DoorWork>().DoorRightCode = "17";
+                    player.GetComponent<DoorWork>().DoorDownCode = "";
+                    break;
+                case "17":
+                    player.GetComponent<DoorWork>().DoorLeftCode = "16";
+                    player.GetComponent<DoorWork>().DoorUpCode = "14";
+                    player.GetComponent<DoorWork>().DoorRightCode = "18";
+                    player.GetComponent<DoorWork>().DoorDownCode = "";
+                    break;
+                case "18":
+                    player.GetComponent<DoorWork>().DoorLeftCode = "17";
+                    player.GetComponent<DoorWork>().DoorUpCode = "";
+                    player.GetComponent<DoorWork>().DoorRightCode = "19";
+                    player.GetComponent<DoorWork>().DoorDownCode = "";
+                    break;
+                case "19":
+                    player.GetComponent<DoorWork>().DoorLeftCode = "18";
+                    player.GetComponent<DoorWork>().DoorUpCode = "";
+                    player.GetComponent<DoorWork>().DoorRightCode = "";
+                    player.GetComponent<DoorWork>().DoorDownCode = "";
+                    break;
             }
 
             enterRoom = false;
