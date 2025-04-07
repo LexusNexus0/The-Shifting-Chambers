a01489336d8b1a46ff2627d084f74ff9e9db1d03
LexusNexus <131169483+LexusNexus0@users.noreply.github.com>
2025-04-07 10:58:26 +1000

    Add files via upload

---
+++ Scripts/CameraMoving.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMoving : MonoBehaviour
+{
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.transform.position.x < 5 &&  player.transform.position.x > -5)
+        {
+            transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+        }
+        else if (player.transform.position.x > 5) {
+            transform.position = new Vector3(5, transform.position.y, transform.position.z);
+        }
+        else if (player.transform.position.x < -5) {
+            transform.position = new Vector3(-5, transform.position.y, transform.position.z);
+        }
+    }
+}
